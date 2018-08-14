36280533ba1513f7e8e7ca2d1c54a7f9da7c8cdb
Mary-Kate <marykatj@gmail.com>
2018-08-14 16:33:25 -0400

    additional changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 //Deliverables:
 
 // 1. Get API to render, with song Title & Artist.  When I click a tile, make the lyrics show.
-// 2. When I click an edit button, have Edit form appear to edit Song
-// 3. Get a search bar, to detect songs by artist.  When i click the sort button, the songs should sort based on alphabetical order
-// 4. Be able to create a new song
+// 2. Be able to create a new song
+// 3. Get a search bar, to detect songs by artist.
+// 4. When i click the sort button, the songs should sort based on alphabetical order
+// 5. When I click an edit button, have Edit form appear to edit Song
 
 import React, { Component } from 'react'
 import SongContainer from './SongContainer'
@@ -39,3 +40,4 @@ export default App;
 //MK NOTES: sort, comment, update, delete
 //boolean greased vs non-greased, and toggle the state
 //default props
+//controlled inputs: value={} & onChange={}
